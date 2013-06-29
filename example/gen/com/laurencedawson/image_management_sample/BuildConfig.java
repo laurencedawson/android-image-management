7466289be6eb5421c8a087466ad673aaf9ae09e7
Laurence Dawson <loljdawson@gmail.com>
2013-06-29 01:44:52 +0100

    .gitignore is now setup.

--- example/gen/com/laurencedawson/image_management_sample/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.laurencedawson.image_management_sample;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- library/gen/com/laurencedawson/image_management/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.laurencedawson.image_management;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
